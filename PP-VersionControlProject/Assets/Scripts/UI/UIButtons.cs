d47c8907b9f62f69c5caed36788c88e32419c09f
Zilexion <59101106+XenoZilexion@users.noreply.github.com>
2020-01-31 17:30:51 -0500

    Added Main Menu
    Added a Main Menu with Title, background, 'Play' and 'Quit' options.

---
+++ PP-VersionControlProject/Assets/Scripts/UI/UIButtons.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIButtons : MonoBehaviour
+{
+    public string twoPlayerVersusName;
+    public void PlayTwoPlayerVersus()
+    {
+        SceneManager.LoadScene(twoPlayerVersusName);
+    }
+    public void QuitTheGame()
+    {
+        Application.Quit();
+    }
+}
