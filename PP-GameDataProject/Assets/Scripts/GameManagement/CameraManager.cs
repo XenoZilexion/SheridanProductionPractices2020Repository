73a9309639349f2a38cfe7b7589e26ece32700c9
Zilexion <59101106+XenoZilexion@users.noreply.github.com>
2020-02-27 13:24:43 -0500

    Added Camera System
    Scene Setup and Camera System/Game Management features to change game states added.

---
+++ PP-GameDataProject/Assets/Scripts/GameManagement/CameraManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    #region variables
+    public Transform[] cameraLocations;
+    public enum gameState { MainMenu, Market, Prep, Cooking, Results}
+    public gameState currentState = gameState.MainMenu;
+    #endregion
+
+    public void Update() { 
+        // automatically relocates camera based on gameState 
+        switch (currentState) {
+            case gameState.MainMenu:
+                Camera.main.transform.position = cameraLocations[0].position;
+                break;
+            case gameState.Market:
+                Camera.main.transform.position = cameraLocations[1].position;
+                break;
+            case gameState.Prep:
+                Camera.main.transform.position = cameraLocations[2].position;
+                break;
+            case gameState.Cooking:
+                Camera.main.transform.position = cameraLocations[3].position;
+                break;
+            case gameState.Results:
+                Camera.main.transform.position = cameraLocations[4].position;
+                break;
+        }
+    }
+
+    // public function to change state from other managers
+    public void ChangeCameraLocation(gameState state) {
+        currentState = state;
+    }
+   
+}
---
+++ PP-GameDataProject/Assets/Scripts/UI/UIButtons.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIButtons : MonoBehaviour {
+    // this script is a compilation of very simple UI Button functions in one for convenience.
+    #region variables
+    public string sceneName;
+    public CameraManager cameraReference;
+    #endregion
+    #region functions
+    //scene managing functions
+    public void SceneChange() {
+        SceneManager.LoadScene(sceneName);
+    }
+    public void QuitTheGame() {
+        Application.Quit();
+    }
+    //tie in functions for other game management functions to change game 'phase'
+    public void SetGameStateMainMenu() {
+        cameraReference.ChangeCameraLocation(CameraManager.gameState.MainMenu);
+    }
+    public void SetGameStateMarket() {
+        cameraReference.ChangeCameraLocation(CameraManager.gameState.Market);
+    }
+    public void SetGameStatePreparation() {
+        cameraReference.ChangeCameraLocation(CameraManager.gameState.Prep);
+    }
+    public void SetGameStateCooking() {
+        cameraReference.ChangeCameraLocation(CameraManager.gameState.Cooking);
+    }
+    public void SetGameStateResults() {
+        cameraReference.ChangeCameraLocation(CameraManager.gameState.Results);
+    }
+    #endregion
+}
