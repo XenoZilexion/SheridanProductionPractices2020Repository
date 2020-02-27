ff33fe2641abe7aef799250dedeb51f9ccbdd658
Zilexion <59101106+XenoZilexion@users.noreply.github.com>
2020-02-27 16:54:43 -0500

    Added Gold Counter
    Added UI Counter to Communicate Current Gold Count to the Player

---
+++ PP-GameDataProject/Assets/Scripts/UI/UIGoldCounter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIGoldCounter : MonoBehaviour {
+
+    #region variables
+    //reference to gold count
+    public Inventory inventoryReference;
+    //self references
+    public Text counter;
+    public Color goldColor;
+    public Color brokeColor;
+    #endregion
+
+    #region updates
+    // Update is called once per frame
+    void Update() {
+        // constantly adjust gold counter
+        if (inventoryReference.gold > 0) {
+            counter.color = goldColor;
+        } else {
+            counter.color = brokeColor;
+        }
+            counter.text = "" + inventoryReference.gold;
+    }
+    #endregion
+}
