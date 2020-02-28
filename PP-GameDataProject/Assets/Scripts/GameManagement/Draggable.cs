3423356bcb94071804363ef97e1273ee3f18fb4f
Zilexion <59101106+XenoZilexion@users.noreply.github.com>
2020-02-28 02:07:20 -0500

    Added Food Movement
    Added ability to move food in the kitchen.

--- PP-GameDataProject/Assets/Scripts/GameManagement/Draggable.cs
+++ PP-GameDataProject/Assets/Scripts/GameManagement/Draggable.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Draggable : MonoBehaviour
 {
+
+    bool dragging = false;
+    float xOffset;
+    float yOffset;
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,33 @@ public class Draggable : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (dragging) {
+            Vector3 mousePosition;
+            mousePosition = Input.mousePosition;
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            this.gameObject.transform.position = new Vector3(mousePosition.x - xOffset, mousePosition.y - yOffset, this.transform.position.z);
+        }
+    }
+
+    private void OnMouseDown() {
+        if (Input.GetMouseButtonDown(0)) {
+            Vector3 mousePosition;
+            mousePosition = Input.mousePosition;
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+            xOffset = mousePosition.x - this.transform.localPosition.x;
+            yOffset = mousePosition.y - this.transform.localPosition.y;
+
+            dragging = true;
+        }
+       
+    }
+
+    private void OnMouseDrag() {
         
+    }
+
+    private void OnMouseUp() {
+        dragging = false;
     }
 }
